7891addd0894b4c332460c2f34868a694d629706
Dev-Abdalla-Magdy <you@example.com>
2023-04-29 21:44:51 +0300

    Added Click Button And Scroller

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
+/* Start List Show And Hidden */
+
 let icon = document.getElementById("icon");
 let links = document.getElementById("links");
 icon.addEventListener("click",()=>{
-  links.style.display = "block";
-  icon.addEventListener("click",()=>{
-    if(links.style.display = "block"){
-      links.style.display = "none";
-      icon.addEventListener("click",()=>{
-        if(links.style.display = "none"){
-          links.style.display = "block";
-        }
-      })
-    }
-  })
+  links.classList.toggle("hide");
 });
+
+/* End List Show And Hidden */
+
+/* Start Scroller */
+
+let scroll = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", ()=>{
+  let scrollTop = document.documentElement.scrollTop;
+	scroll.style.width = `${((scrollTop / height) * 100)}%`;
+})
+
+/* End Scroller */
+
+/* Start Social Links */
