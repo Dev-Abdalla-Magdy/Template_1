d5a8fff19e547d2600406ba87611f46982745610
Dev-Abdalla-Magdy <you@example.com>
2023-04-29 22:24:03 +0300

    Solving Proplem List Button

--- js/main.js
+++ js/main.js
@@ -2,8 +2,16 @@
 
 let icon = document.getElementById("icon");
 let links = document.getElementById("links");
+
 icon.addEventListener("click",()=>{
-  links.classList.toggle("hide");
+  if(links.classList.contains("hide")){
+    links.classList.remove("hide");
+    links.classList.add("show");
+  }
+  else if(links.classList.contains("show")){
+    links.classList.add("hide");
+    links.classList.remove("show");
+  }
 });
 
 /* End List Show And Hidden */
